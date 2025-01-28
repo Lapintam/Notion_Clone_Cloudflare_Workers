394ddc71909f0ea438453914a3c80fa35f965772
MuRL <73443780+Lapintam@users.noreply.github.com>
2025-01-28 17:26:39 -0500

    created first AI translation cloudflare worker

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,42 @@ import OpenAI from 'openai';
 import { Hono } from 'hono';
 import { cors } from 'hono/cors';
 
-export default {
-	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
-	},
-} satisfies ExportedHandler<Env>;
+type Bindings = {
+	OPEN_AI_KEY: string;
+	AI: Ai;
+}
+
+const app = new Hono<{Bindings: Bindings}>();
+
+app.use(
+	'/*',
+	cors({
+		origin: '*', // Allow requests from your Next.js app
+		allowHeaders: ['X-Custom-Header', 'Upgrade-Insecure-Requests', 'Content-Type'], // Add Content-Type to the allowed headers to fix CORS
+		allowMethods: ['GET', 'POST', 'PUT','OPTIONS'],
+		exposeHeaders: ['Content-Range', 'X-Kuma-Revision'],
+		maxAge: 600,
+		credentials: true,
+	})
+);
+
+app.post('/translateDocument', async (c) => {
+	const { documentData, targetLang } = await c.req.json();
+
+	// Generate a summary of the document
+	const summaryResponse = await c.env.AI.run('@cf/facebook/bart-large-cnn', {
+		input_text: documentData,
+		max_length: 1000,
+	});
+
+	// translate the summary into another language
+	const response = await c.env.AI.run('@cf/meta/m2m100-1.2b', {
+		text: summaryResponse.summary,
+		source_lang: 'english',
+		target_lang: targetLang,
+	})
+
+	return new Response(JSON.stringify(response));
+})
+
+export default app;
